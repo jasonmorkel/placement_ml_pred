360364c05a432107f2a4027f13588c97e6b51653
Jason Morkel <jkmorkel@gmail.com>
2021-09-21 09:43:01 +0200

    master

---
+++ placement_ml_pred/test.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+import os
+os.chdir('placement_ml_pred')
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import metrics
+
+#load_data
+x_train = pd.read_csv('x_train.csv')
+y_train = pd.read_csv('y_train.csv')
+x_test = pd.read_csv('x_test.csv')
+y_test = pd.read_csv('y_test.csv')
+
+#Create a Gaussian Classifier
+clf=RandomForestClassifier(n_estimators=100)
+
+#Train the model using the training sets y_pred=clf.predict(X_test)
+clf.fit(x_train,y_train)
+
+#test_model
+y_pred=clf.predict(x_test)
+
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
